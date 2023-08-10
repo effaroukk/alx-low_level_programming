d1bb0fe4df36d842f26aa4dbf05cd4202317f721
root <root@DESKTOP-TG4LC6T.localdomain>
2023-08-10 19:18:58 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Printing a line with a puts function programming
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

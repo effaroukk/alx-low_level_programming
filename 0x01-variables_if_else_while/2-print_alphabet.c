a012a0b02e9194a8bf284ecee22a140e03c7614a
umar farouk abdulai <efqay1@gmail.com>
2023-08-11 16:51:34 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char d;
+
+	d = 'b';
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}
+			putchar('\n');
+	return (0);
+}

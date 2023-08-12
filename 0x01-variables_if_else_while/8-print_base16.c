abdfaf49e23844649ee780e8d496713fa407fb80
umar farouk abdulai <efqay1@gmail.com>
2023-08-11 19:32:32 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char j;
+	int i;
+
+	j = 'a';
+	i = 0;
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	while
+		(j <= 'p') {
+			putchar(j);
+			j++;
+		}
+	putchar('\n');
+	return (0);
+}

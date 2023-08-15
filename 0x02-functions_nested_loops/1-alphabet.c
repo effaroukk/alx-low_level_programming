4c1793051a5c90a131af2b3a6a5595a31ef128d8
umar farouk abdulai <efqay1@gmail.com>
2023-08-15 10:46:51 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - printing alphabets in low case
+ */
+void print_alphabet(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++);
+       	_putchar(letter);
+
+	_putchar('\n');
+}
